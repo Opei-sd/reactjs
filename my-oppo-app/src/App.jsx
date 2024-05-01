354d3e0f971a2f152a4fe6df88b4f4f0f066a144
Opeodu Ibrahim <temitopeopeodu2005@gmail.com>
2024-05-01 17:40:10 +0100

    my reactjs edis on appjsx

--- my-oppo-app/src/App.jsx
+++ my-oppo-app/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Shoppinglist = [
+  {
+    id: 1,
+    name: "Milk",
+    imageUrl:
+      "https://cdn.britannica.com/77/200377-050-4326767F/milk-splashing-glass.jpg",
+    imageSize: 60,
+  },
+  {
+    id: 2,
+    name: "Bread",
+    imageUrl:
+      "https://img.freepik.com/premium-photo/slices-bread-isolated-white-background-top-view_530649-32.jpg",
+    imageSize: 60,
+  },
+  {
+    id: 3,
+    name: "Indomie",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI65YictgmbNvKbL8Yn5miyN841BkAwxsiA2PKxAPoWg&s",
+    imageSize: 60,
+  },
+  {
+    id: 4,
+    name: "Butter",
+    imageUrl:
+      "https://c8.alamy.com/comp/BXRXHJ/unsalted-butter-close-up-BXRXHJ.jpg",
+    imageSize: 60,
+  },
+  {
+    id: 5,
+    name: "Eggs",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjBg2jsXpnpXFdr5P97SS5-a64eSdHj-WjHl-CRjQoRQ&s",
+    imageSize: 60,
+  },
+  {
+    id: 6,
+    name: "Yogurt",
+    imageUrl:
+      "https://media.istockphoto.com/id/159758583/photo/strawberry-dessert-in-a-clear-bowl.jpg?s=612x612&w=0&k=20&c=uJwhiltEWEq10farUFcPPiNBRz3Qr7-Hm00QVSwUSdc=",
+    imageSize: 60,
+  },
+];
+
+export default function App() {
+  const listItems = Shoppinglist.map((item) => (
+    <li key={item.id}>
+      <img src={item.imageUrl} alt={item.name} />
+      {item.name}
+    </li>
+  ));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Shopping List</h1>
+      <ul>
+        {Shoppinglist.map((item) => (
+          <li key={item.id}>
+            <img src={item.imageUrl} alt={item.name} />
+            {item.name}
+          </li>
+        ))}
+      </ul>
 
-export default App
+      <img
+        className="App-logo"
+        src="Shppinglist.imageUrl"
+        alt="Photo of Shoppinglist.name"
+        style={{
+          width: Shoppinglist.imageSize,
+          height: Shoppinglist.imageSize,
+        }}
+      ></img>
+    </div>
+  );
+}
